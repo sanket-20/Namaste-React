094bc09e1ff1e852aee49520ec2a931751c2ff12
sanket-20 <sanketgupte20@gmail.com>
2023-06-16 22:04:54 +0530

    add more content

--- day-5/src/components/Body.js
+++ day-5/src/components/Body.js
@@ -7,29 +7,68 @@ import RestaurantCard from "./RestaurantCard";
 // what is hook?
 // what is useState()
 
+function filterData(searchText, restaurants) {
+    const filterData = restaurants.filter((restaurant) =>
+        restaurant.data.name.includes(searchText)
+    );
+
+    return filterData;
+}
+
+
 const Body = () => {
     // const searchText = "KFC";
 
     // searchText is local state var
-    const [searchInput, setSearchInput] = useState("KFC");
+    // const [searchInput, setSearchInput] = useState("KFC");
+
+
+    // const  searchvar = useState("KFC");//return [var_name, fun to update the var]
+
+    // const [ searchText, setSearchText] = searchvar;
+    // const stext = "KFC";
+    const [restaurants, setRestaurants] = useState(restaurantList);
+    const [searchText, setSearchText] = useState("");//return [var_name, fun to update the var]
+
+    // eg
+    // const searchClicked = false;
+    // const [searchClicked, setSearchClicked] = useState("false");
 
     return (
         <>
             <div className="search-container">
-                <input 
-                    type="text" 
-                    className="search-input" 
+                <input
+                    type="text"
+                    className="search-input"
                     placeholder="Search"
-                    value={searchInput}
-                    onChange={(e)=>{
-                        setSearchInput(e.target.value);
+                    value={searchText}
+                    onChange={(e) => {
+                        setSearchText(e.target.value);
                     }}
                 />
-                <button className="search-btn">Search</button>
+                <button className="search-btn" onClick={() => {
+                    // need to filter the data
+                    const data = filterData(searchText, restaurants);
+                    // update the state - restaurants
+                    setRestaurants(data);
+                }}>Search</button>
+                
+                {/* <h1>{searchText}</h1> */}
             </div>
+            {/* <h1>{searchClicked}</h1>
+            <button onClick={()=>{
+                if(searchClicked === "true"){
+                    setSearchClicked("false");
+                }
+                else{
+                    setSearchClicked("true");
+                }
+            }}>Search</button> */}
+
+
             <div className="restaurant-list">
                 {
-                    restaurantList.map((restaurant) => {
+                    restaurants.map((restaurant) => {
                         return <RestaurantCard {...restaurant.data} />
                     })
                 }
